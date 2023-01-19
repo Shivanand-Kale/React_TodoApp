99233965718a0604fb8bb2c9c17bc17ca08bd525
Shivanand-Kale <117525161+Shivanand-Kale@users.noreply.github.com>
2023-01-19 23:48:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./Components/TodoApp/Todo";
+
+// import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
